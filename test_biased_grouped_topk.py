523c72e9045bf07305b61da781ffc9d426f59bc6
mingfeima <mingfei.ma@intel.com>
2025-03-17 22:18:23 -0700

    add test case for int8_scaled_mm and biased_grouped_topk

---
+++ test_biased_grouped_topk.py
@@ -0,0 +1,95 @@
+import torch
+from sgl_kernel.common_ops import biased_grouped_topk_cpu as grouped_topk
+
+from utils import compare
+
+torch.manual_seed(1111)
+
+# DeepSeek V2/V3/R1 uses biased_grouped_top
+def biased_grouped_topk(
+    hidden_states: torch.Tensor,
+    gating_output: torch.Tensor,
+    correction_bias: torch.Tensor,
+    topk: int,
+    renormalize: bool,
+    num_expert_group: int = 0,
+    topk_group: int = 0,
+):
+    assert hidden_states.shape[0] == gating_output.shape[0], "Number of tokens mismatch"
+
+    scores = gating_output.sigmoid()
+    num_token = scores.shape[0]
+    scores_for_choice = scores.view(num_token, -1) + correction_bias.unsqueeze(0)
+    group_scores = (
+        scores_for_choice.view(num_token, num_expert_group, -1)
+        .topk(2, dim=-1)[0]
+        .sum(dim=-1)
+    )  # [n, n_group]
+    group_idx = torch.topk(group_scores, k=topk_group, dim=-1, sorted=False)[
+        1
+    ]  # [n, top_k_group]
+    group_mask = torch.zeros_like(group_scores)  # [n, n_group]
+    group_mask.scatter_(1, group_idx, 1)  # [n, n_group]
+    score_mask = (
+        group_mask.unsqueeze(-1)
+        .expand(num_token, num_expert_group, scores.shape[-1] // num_expert_group)
+        .reshape(num_token, -1)
+    )  # [n, e]
+    tmp_scores = scores_for_choice.masked_fill(
+        ~score_mask.bool(), float("-inf")
+    )  # [n, e]
+    _, topk_ids = torch.topk(tmp_scores, k=topk, dim=-1, sorted=False)
+    topk_weights = scores.gather(1, topk_ids)
+
+    if renormalize:
+        topk_weights = topk_weights / topk_weights.sum(dim=-1, keepdim=True)
+
+    return topk_weights.to(torch.float32), topk_ids.to(torch.int32)
+
+
+def run_single_test(M, E, G, topk, topk_group, renormalize, dtype):
+
+    # expand gating_output by M, otherwise bfloat16 fall into same value aftering truncating
+    hidden_states = torch.randn(M, 100, dtype=dtype)
+    gating_output = torch.randn(M, E, dtype=dtype) * 2 * M
+    correction_bias = torch.randn(E, dtype=torch.float32)
+
+    ref_topk_weights, ref_topk_ids = biased_grouped_topk(
+        hidden_states.float(),
+        gating_output.float(),
+        correction_bias,
+        topk,
+        renormalize,
+        G,
+        topk_group)
+
+    # fused version
+    topk_weights = torch.empty(
+        M, topk, dtype=torch.float32, device=hidden_states.device
+    )
+    topk_ids = torch.empty(M, topk, dtype=torch.int32, device=hidden_states.device)
+    grouped_topk(
+        topk_weights,
+        topk_ids,
+        hidden_states,
+        gating_output,
+        correction_bias,
+        topk,
+        renormalize,
+        G,
+        topk_group)
+
+    #print(ref_topk_ids)
+    #print(ref_topk_weights)
+    #print(topk_ids)
+    #print(topk_weights)
+
+    res = torch.zeros(M, E, dtype=torch.float)
+    ref = torch.zeros(M, E, dtype=torch.float)
+    res.scatter_(1, topk_ids.long(), topk_weights)
+    ref.scatter_(1, ref_topk_ids.long(), ref_topk_weights)
+    compare(res, ref, False)
+
+
+for renormalize in [True, False]:
+    run_single_test(11, 256, 8, 8, 2, renormalize, torch.bfloat16)
---
+++ test_gemm_int8.py
@@ -0,0 +1,87 @@
+import torch
+from sgl_kernel.common_ops import int8_scaled_mm_cpu
+from sgl_kernel.common_ops import convert_weight_packed
+
+from utils import compare
+
+torch.manual_seed(1111)
+
+#torch.set_printoptions(profile="full")
+
+def per_token_quant_int8(x):
+    x = x.float()
+    absmax = x.abs().max(dim=-1).values
+    absmax = absmax.clamp_min(1e-10).unsqueeze(-1)
+    scale_x = absmax / 127
+    x_q = x.mul(127 / absmax)
+    x_q = torch.round(x_q).to(torch.int8)
+
+    return x_q, scale_x
+
+
+def native_w8a8_per_token_matmul(A, B, As, Bs, bias, output_dtype=torch.bfloat16):
+    """Matrix multiplication function that supports per-token input quantization and per-column weight quantization"""
+    A = A.to(torch.float32)
+    B = B.to(torch.float32)
+
+    assert A.shape[-1] == B.shape[-1], "Dimension mismatch"
+    assert B.ndim == 2 and B.is_contiguous(), "B must be a 2D contiguous tensor"
+
+    # Reshape input
+    M = A.numel() // A.shape[-1]
+    B = B.t()  # Transpose weight matrix
+    N, K = B.shape
+    origin_C_shape = A.shape[:-1] + (K,)
+    A = A.reshape(M, N)
+
+    # As is per-token [M, 1], Bs is per-column [1, K]
+    C = torch.matmul(A, B)  # [M, K]
+    C = As * C * Bs.view(1, -1)  # Broadcast per-column scale
+
+    if bias is not None:
+        C.add_(bias.view(1, -1))
+
+    return C.reshape(origin_C_shape).to(output_dtype)
+
+
+def run_single_test(M, N, K, dtype, has_bias=False):
+
+    A = torch.randn((M, K), dtype=dtype) / 10
+    Aq, As = per_token_quant_int8(A)
+
+    factor_for_scale = 1e-2
+    int8_max = 127
+    int8_min = -128
+
+    B = (torch.rand((N, K), dtype=torch.float32) - 0.5) * 2
+    Bq = (B * int8_max).clamp(min=int8_min, max=int8_max).to(torch.int8)
+    Bs = torch.rand(N) * factor_for_scale
+
+    bias = torch.randn(N) if has_bias else None
+
+    ref_out = native_w8a8_per_token_matmul(Aq, Bq, As, Bs, bias, dtype)
+    out = int8_scaled_mm_cpu(A, Bq, Bs, bias if has_bias else None, False);
+
+    compare(ref_out, out)
+
+
+for bias in [True, False]:
+    run_single_test(12, 32 * 12, 32 * 17, torch.bfloat16, bias)
+    run_single_test(1, 32, 32, torch.bfloat16, bias)
+
+def test_weight_prepack(oc, ic):
+
+    BLOCK_N = 32
+
+    off = torch.randint(low=-128, high=128, size=(1, ic), dtype=torch.int32).fill_(128)
+    w = torch.randint(low=-128, high=128, size=(oc, ic), dtype=torch.int8)
+    packed_w = convert_weight_packed(w)
+
+    comp = torch.matmul(off, w.t().to(torch.int32))
+    packed_w1 = packed_w.view(-1).narrow(0, 0, oc * ic).view(oc, ic)
+
+    ref = w.view(int(oc/BLOCK_N), BLOCK_N, int(ic/4), 4).permute(0, 2, 1, 3).contiguous().view(oc, ic)
+
+    print("\n### test_weight_prepack: ", torch.equal(ref, packed_w1))
+
+#test_weight_prepack(32, 32)
