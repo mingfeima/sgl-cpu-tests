93eb41678e43eede4080dcdb6de74a2efd5a4b5f
mingfeima <mingfei.ma@intel.com>
2025-12-09 09:50:23 +0000

    minor change

--- run_image_cpu.sh
+++ run_image_cpu.sh
@@ -1,16 +1,15 @@
 
 # jemalloc:
 export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:-1,muzzy_decay_ms:-1";
-export LD_PRELOAD=/home/mingfeim/jemalloc-5.3.0/lib/libjemalloc.so
+export LD_PRELOAD=/home/mingfeima/packages/jemalloc-5.3.0/lib/libjemalloc.so
 
-CORES=32
-LAST_CORE=`expr 60 + $CORES - 1`
-PREFIX="numactl --physcpubind=60-$LAST_CORE --membind=1"
+CORES=40
+FIRST_CORE=120
+LAST_CORE=`expr $FIRST_CORE + $CORES - 1`
+PREFIX="numactl --physcpubind=$FIRST_CORE-$LAST_CORE --membind=3"
 echo -e "### using $PREFIX\n"
 
-#OMP_NUM_THREADS=$CORES $PREFIX python3 ./bench_image_processing.py
-
-export ATEN_CPU_CAPABILITY=avx2
+#export ATEN_CPU_CAPABILITY=avx2
 
 # enable profile and validate
 #OMP_NUM_THREADS=$CORES $PREFIX python3 ./bench_image_processing.py --profile --validate
