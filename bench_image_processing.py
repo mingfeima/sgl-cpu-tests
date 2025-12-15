1587428f1422def20033ab7f64afa25fc93d539c
mingfeima <mingfei.ma@intel.com>
2025-12-15 16:05:27 +0000

    change interface of image preprocessor benchmark

--- bench_image_processing.py
+++ bench_image_processing.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from transformers import AutoImageProcessor
+from typing import Union, Sequence, Dict, Any
 import torch
 import torch.nn.functional as F
 import numpy as np
@@ -7,9 +8,18 @@ import numpy as np
 import argparse
 from time import time
 
+import sgl_kernel
+image_preprocess_cpu = torch.ops.sgl_kernel.image_preprocess_cpu
+
 # set seed
 np.random.seed(1234)
 torch.manual_seed(1234)
+
+# precision
+pres = {
+    torch.bfloat16 : 1e-2,
+    torch.float32: 1e-5,
+}
 
 def _prepare_image_like_inputs(images, do_convert_rgb):
     def _process_image(image, do_convert_rgb):
@@ -75,23 +85,27 @@ def normalize(tensor: torch.Tensor, mean: list[float], std: list[float], inplace
         std = std.view(-1, 1, 1)
     return tensor.sub_(mean).div_(std)
 
-# reference
 def qwen2vl_preprocess_torch(
-    images,
-    do_convert_rgb=True,
-    do_resize=True,
-    size={"shortest_edge": 65536, "longest_edge": 16777216},
-    interpolation="bicubic",
-    do_rescale=True,
-    rescale_factor=1/255,
-    do_normalize=True,
-    image_mean=[0.5,0.5,0.5],
-    image_std=[0.5,0.5,0.5],
-    patch_size=16,
-    temporal_patch_size=2,
-    merge_size=2,
-    disable_grouping=True,
-):
+    images: Union[torch.Tensor, Sequence[torch.Tensor]],
+    do_convert_rgb: bool = True,
+    do_resize: bool = True,
+    size: Dict[str, int] = {"shortest_edge": 65536, "longest_edge": 16777216},
+    interpolation: str = "bicubic",
+    do_rescale: bool = True,
+    rescale_factor: float = 1/255,
+    do_normalize: bool = True,
+    image_mean: Sequence[float] = [0.5, 0.5, 0.5],
+    image_std: Sequence[float] = [0.5, 0.5, 0.5],
+    patch_size: int = 16,
+    temporal_patch_size: int = 2,
+    merge_size: int = 2,
+    disable_grouping: bool = True,
+    out_dtype: torch.dtype = torch.float,
+) -> Dict[str, torch.Tensor]:
+    """
+    Preprocess images for Qwen2VL model in PyTorch.
+    """
+
     if not isinstance(images, list):
         images = [images]
 
@@ -164,7 +178,54 @@ def qwen2vl_preprocess_torch(
     pixel_values = torch.cat(processed_images, dim=0)
     image_grid_thw = torch.tensor(processed_grids)
 
+    return {"pixel_values": pixel_values.to(out_dtype), "image_grid_thw": image_grid_thw}
+
+def qwen2vl_preprocess_cpu(
+    images: Union[torch.Tensor, Sequence[torch.Tensor]],
+    do_convert_rgb: bool = True,
+    do_resize: bool = True,
+    size: Dict[str, int] = {"shortest_edge": 65536, "longest_edge": 16777216},
+    interpolation: str = "bicubic",
+    do_rescale: bool = True,
+    rescale_factor: float = 1/255,
+    do_normalize: bool = True,
+    image_mean: Sequence[float] = [0.5, 0.5, 0.5],
+    image_std: Sequence[float] = [0.5, 0.5, 0.5],
+    patch_size: int = 16,
+    temporal_patch_size: int = 2,
+    merge_size: int = 2,
+    disable_grouping: bool = True,
+    out_dtype: torch.dtype = torch.float,
+) -> Dict[str, torch.Tensor]:
+    """
+    Preprocess images for Qwen2VL model in optimized CPU kernels.
+    """
+
+    if not isinstance(images, list):
+        images = [images]
+
+    images = _prepare_image_like_inputs(images, do_convert_rgb)
+
+    pixel_values, image_grid_thw = image_preprocess_cpu(
+        images,
+        do_convert_rgb,
+        do_resize,
+        size["shortest_edge"],
+        size["longest_edge"],
+        interpolation,
+        do_rescale,
+        rescale_factor,
+        do_normalize,
+        [0.5, 0.5, 0.5],
+        [0.5, 0.5, 0.5],
+        patch_size,
+        temporal_patch_size,
+        merge_size,
+        disable_grouping,
+        out_dtype)
+
     return {"pixel_values": pixel_values, "image_grid_thw": image_grid_thw}
+
 
 def create_random_image(min_size=1365, max_size=2048):
     """ Randomly create image"""
@@ -174,10 +235,13 @@ def create_random_image(min_size=1365, max_size=2048):
     return Image.fromarray(img_array)
 
 
-def run_single_benchmark(processor, min_size, max_size, nwarmup, niters, batch_size, validate):
+def run_single_benchmark(processor, bfloat16, min_size, max_size, nwarmup, niters, batch_size, validate):
 
-    print("\n### Run image preprocessor benchmark with {:d} images".format(batch_size))
+    out_dtype = torch.bfloat16 if bfloat16 else torch.float32
+    print(f"\n### Run image preprocessor benchmark with {batch_size:d} images with out dtype",out_dtype)
+    print("### Image Min Size ", min_size, " and Max Size ", max_size)
 
+    nwarmp = max(nwarmup // batch_size, 5)
     niters = niters // batch_size
     assert niters > 0, f"niters too small for batch_size={batch_size}"
 
@@ -185,40 +249,58 @@ def run_single_benchmark(processor, min_size, max_size, nwarmup, niters, batch_s
     for i in range(batch_size):
         img = create_random_image(min_size, max_size)
         images.append(img)
-        print(f"  Image {i}: {img.size}")
 
     # do warmups
     for _ in range(nwarmup):
         image_processor = processor.preprocess(images=images, return_tensors="pt")
+        if bfloat16 and isinstance(image_processor.get("pixel_values"), torch.Tensor):
+            image_processor["pixel_values"] = image_processor["pixel_values"].to(torch.bfloat16)
 
     # do benchmark
     start = time()
     for _ in range(niters):
         image_processor = processor.preprocess(images=images, return_tensors="pt")
+        if bfloat16 and isinstance(image_processor.get("pixel_values"), torch.Tensor):
+            image_processor["pixel_values"] = image_processor["pixel_values"].to(torch.bfloat16)
     end = time()
     total_time = (end - start) * 1000 / niters # ms
     avg_time = total_time / batch_size
 
+    # do warmups
+    for _ in range(nwarmup):
+        image_processor_cpu = qwen2vl_preprocess_cpu(images, out_dtype=out_dtype)
+
+    # do benchmark
+    start = time()
+    for _ in range(niters):
+        image_processor_cpu = qwen2vl_preprocess_cpu(images, out_dtype=out_dtype)
+    end = time()
+    total_time_cpu = (end - start) * 1000 / niters # ms
+    avg_time_cpu = total_time_cpu / batch_size
+
     print("")
     print("  Output shape :", image_processor['pixel_values'].shape, image_processor['image_grid_thw'].shape)
-    print("  Average time : {:.3f} ms".format(avg_time))
-    print("  Total time   : {:.3f} ms".format(total_time))
+    print("  Output dtype :", image_processor['pixel_values'].dtype, image_processor['image_grid_thw'].dtype)
+    print("  Average time : {:.3f} ms v.s. {:.3f} ms".format(avg_time, avg_time_cpu))
+    print("  Total time   : {:.3f} ms v.s. {:.3f} ms".format(total_time, total_time_cpu))
 
     if validate:
-        image_processor_torch = qwen2vl_preprocess_torch(images)
+        image_processor_torch = qwen2vl_preprocess_torch(images, out_dtype=out_dtype)
 
-        atol, rtol = 1e-5, 1e-5
+        atol = rtol = pres[out_dtype]
         res = torch.allclose(image_processor['pixel_values'], image_processor_torch['pixel_values'], rtol=rtol, atol=atol)
-        print("")
-        print("### validating: image_processor['pixel_values']: ", res)
         res2 = torch.equal(image_processor['image_grid_thw'], image_processor_torch['image_grid_thw'])
-        print("### validating: image_processor['image_grid_thw']: ", res2)
+        print("\n### validating original preprocessor with pure torch preprocessor : ", res, res2)
+
+        res = torch.allclose(image_processor['pixel_values'], image_processor_cpu['pixel_values'], rtol=rtol, atol=atol)
+        res2 = torch.equal(image_processor['image_grid_thw'], image_processor_cpu['image_grid_thw'])
+        print("### validating original preprocessor with cpu optimized preprocessor : ", res, res2)
 
 
-def benchmark_image_preprocessor(processor, min_size, max_size, nwarmup, niters, batches, profile, validate):
+def benchmark_image_preprocessor(processor, bfloat16, min_size, max_size, nwarmup, niters, batches, profile, validate):
     for batch_size in batches:
         with torch.autograd.profiler.profile(enabled=profile) as prof:
-            run_single_benchmark(processor, min_size, max_size, nwarmup, niters, batch_size, validate)
+            run_single_benchmark(processor, bfloat16, min_size, max_size, nwarmup, niters, batch_size, validate)
 
         if profile:
             print(prof.key_averages().table(sort_by="cpu_time_total"))
@@ -226,11 +308,12 @@ def benchmark_image_preprocessor(processor, min_size, max_size, nwarmup, niters,
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--bfloat16", action='store_true', default=False)
     parser.add_argument("--min", type=int, default=1365)
     parser.add_argument("--max", type=int, default=2048)
     parser.add_argument("--nwarmup", type=int, default=20)
     parser.add_argument("--niters", type=int, default=200)
-    parser.add_argument("--batches", type=int, nargs='+', default=[5])
+    parser.add_argument("--batches", type=int, nargs='+', default=[20])
     parser.add_argument("--model_path", type=str, default="./models/Qwen3-VL-30B-A3B-Instruct")
     parser.add_argument("--profile", action='store_true', default=False)
     parser.add_argument("--validate", action='store_true', default=False)
@@ -241,6 +324,7 @@ if __name__ == "__main__":
 
     benchmark_image_preprocessor(
         processor,
+        args.bfloat16,
         args.min,
         args.max,
         args.nwarmup,
--- run_image_cpu.sh
+++ run_image_cpu.sh
@@ -3,14 +3,23 @@
 export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:-1,muzzy_decay_ms:-1";
 export LD_PRELOAD=/home/mingfeima/packages/jemalloc-5.3.0/lib/libjemalloc.so
 
+
+# change this according to local machine setting
+# using 3rd numa node with 40 cores
 CORES=40
 FIRST_CORE=120
 LAST_CORE=`expr $FIRST_CORE + $CORES - 1`
 PREFIX="numactl --physcpubind=$FIRST_CORE-$LAST_CORE --membind=3"
 echo -e "### using $PREFIX\n"
 
-#export ATEN_CPU_CAPABILITY=avx2
+
+# exaples to run:
 
 # enable profile and validate
 #OMP_NUM_THREADS=$CORES $PREFIX python3 ./bench_image_processing.py --profile --validate
+
+# enable validate
 OMP_NUM_THREADS=$CORES $PREFIX python3 ./bench_image_processing.py --validate
+
+# enable validate and fuse bfloat16 conversion
+#OMP_NUM_THREADS=$CORES $PREFIX python3 ./bench_image_processing.py --validate --bfloat16
